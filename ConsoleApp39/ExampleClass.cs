f1196817b507c8b0413e45ccba42a0f60269c4c9
Sparc-jr <ant-v@bk.ru>
2022-12-19 16:43:25 +0300

    Add project files.

---
+++ ConsoleApp39/ExampleClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    internal class ExampleClass
+    {
+        private string carModel;
+        private string carColor;
+        private string ownerName;
+        public string CarModel { get; set; }
+        public string CarColor { get; set; }
+        public string OwnerName { get; set; }
+        public static List<ExampleClass> _cars;
+        public ExampleClass(string model, string color, string owner)
+        {
+            if (_cars == null)
+                _cars = new List<ExampleClass>();
+            this.CarModel = model;
+            this.CarColor = color;
+            this.OwnerName = owner;
+            foreach (var car in _cars)
+            {
+                car.OwnerName = owner;
+            }
+            _cars.Add(this);
+        }
+    }
+}
---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp39;
+
+class Program
+{
+    static void Main()
+    {
+        
+        var cars = new ExampleClass[3]; 
+        for (int i = 0; i < 3;i++)
+        {
+            
+            var carModel = Console.ReadLine();
+            var carColor = Console.ReadLine();
+            var carOwner = Console.ReadLine();
+            cars[i] = new ExampleClass(carModel, carColor, carOwner);
+        }
+             
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.CarModel},  {car.CarColor}  -  {car.OwnerName}");
+        }
+
+    }
+
+
+}
+
